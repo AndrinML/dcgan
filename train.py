9a5c3bb2f7c0727a4fedbd88bebf72b5ca8be00c
Andrin Jenal <ajenal@ethz.ch>
2017-04-09 13:32:06 +0200

    change defaults

--- train.py
+++ train.py
@@ -12,11 +12,11 @@ from visualizer import ImageVisualizer
 
 flags = tf.app.flags
 
-flags.DEFINE_string("dataset", "datasets/celeb_dataset.h5", "sample results dir")
+flags.DEFINE_string("dataset", "datasets/tree_skel_all_1500_100_1v_64x64.h5", "sample results dir")
 flags.DEFINE_string("data_dir", "results/", "checkpoint and logging results dir")
 flags.DEFINE_integer("batch_size", 128, "batch size")
 flags.DEFINE_integer("image_size", 64, "image size")
-flags.DEFINE_integer("channels", 3, "color channels")
+flags.DEFINE_integer("channels", 1, "color channels")
 flags.DEFINE_integer("max_epoch", 500, "max epoch")
 flags.DEFINE_integer("z_size", 256, "size of latent (feature?) space")
 flags.DEFINE_float("learning_rate", 5e-4, "learning rate")
@@ -30,9 +30,11 @@ def main(_):
     # the checkpoint saver, can create checkpoint files, which later can be use to restore a model state, but it also
     # audits the model progress to a log file
     checkpoint_saver = CheckpointSaver(FLAGS.data_dir)
+    checkpoint_saver.save_experiment_config(FLAGS.__dict__['__flags'])
 
     # load training data
-    train_data, training_data_shape = hdf5_dataset.read_data_set(FLAGS.dataset, image_size=FLAGS.image_size, shape=(FLAGS.image_size, FLAGS.image_size, FLAGS.channels), binarized=False, validation=0).train
+    data_set, data_set_shape = hdf5_dataset.read_data_set(FLAGS.dataset, image_size=FLAGS.image_size, shape=(FLAGS.image_size, FLAGS.image_size, FLAGS.channels), binarized=True, validation=0)
+    train_data = data_set.train
 
     # create a data visualizer
     visualizer = ImageVisualizer(checkpoint_saver.get_experiment_dir(), image_size=FLAGS.image_size)
@@ -42,7 +44,6 @@ def main(_):
     dcgan_model = DCGAN(FLAGS.image_size, FLAGS.channels, z_size=FLAGS.z_size, learning_rate=FLAGS.learning_rate)
 
     print("start", type(dcgan_model).__name__, "model training")
-
     with tf.Session() as sess:
 
         sess.run(tf.global_variables_initializer())
