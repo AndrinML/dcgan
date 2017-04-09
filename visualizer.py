3e6cd42f2098a844723e9038e619708a914a35f2
Andrin Jenal <ajenal@ethz.ch>
2017-04-09 19:39:12 +0200

    adapt visualizer to image range

--- visualizer.py
+++ visualizer.py
@@ -18,22 +18,20 @@ class ImageVisualizer:
         self.experiment_dir = experiment_dir
         self.image_size = image_size
 
-    def _sigmoid(self, x):
-        return 1.0 / (1.0 + np.exp(-x))
+    def _reverse_tanh(self, x):
+        return (x - x.min()) * (1.0 / (x.max() - x.min()))
 
     def training_data_sample(self, train_data, save_name="dataset_samples.png"):
         idxs = np.random.randint(0, train_data.length(), self.NUM_SAMPLES)
         rand_imgs = train_data.images[idxs]
-        image_matrix = self._create_image_grid(rand_imgs, self.NUM_SAMPLES, self.image_size, self.image_size, self.ROWS, self.COLS, ypad=2, xpad=2)
-        # save image matrix
+        image_matrix = self._create_image_grid(self._reverse_tanh(rand_imgs), self.NUM_SAMPLES, self.image_size, self.image_size, self.ROWS, self.COLS, ypad=2, xpad=2)
         self._save_image_matrix(image_matrix, save_name)
 
     def save_generated_samples(self, sample_images, epoch):
         # sample reconstruction
-        image_matrix = self._create_image_grid(self._sigmoid(sample_images), sample_images.shape[0], self.image_size, self.image_size, self.ROWS, self.COLS, ypad=2, xpad=2)
+        image_matrix = self._create_image_grid(self._reverse_tanh(sample_images), sample_images.shape[0], self.image_size, self.image_size, self.ROWS, self.COLS, ypad=2, xpad=2)
         name = "epoch_%03d.png" % epoch
         self._save_image_matrix(image_matrix, name)
-
         print("generated and saved images...")
 
     def _create_image_grid(self, x, num_samples, image_height, image_width, rows, cols, ypad=0, xpad=0):
